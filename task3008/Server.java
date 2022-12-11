2d4466d2d2a8e1681bac8adb7b56b41378d6c460
Andrey <bursov@web.de>
2022-12-11 20:15:38 +0100

    Task seven complete

--- task3008/Server.java
+++ task3008/Server.java
@@ -3,8 +3,13 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Enter server port:");
         try (ServerSocket socket =  new ServerSocket(ConsoleHelper.readInt())) {
@@ -18,6 +23,17 @@ public class Server {
             ConsoleHelper.writeMessage("Error occurred while starting or running the server.");
         }
     }
+
+    public static void sendBroadcastMessage(Message message) {
+        connectionMap.forEach((name, connection) -> {
+            try {
+                connection.send(message);
+            } catch (IOException ignored) {
+                ConsoleHelper.writeMessage("Server failed to send message to " + name);
+            }
+        });
+    }
+
     private static class Handler extends Thread {
         private Socket socket;
 

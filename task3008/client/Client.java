a424d579a79a18c294ef9271d73628c622c3e2dc
Andrey <bursov@web.de>
2022-12-13 17:11:42 +0100

    Task thirteen complete

--- task3008/client/Client.java
+++ task3008/client/Client.java
@@ -1,10 +1,48 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+
+import java.io.IOException;
+
+import static com.javarush.task.task30.task3008.MessageType.*;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Enter server address:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Enter server port:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName()  {
+        ConsoleHelper.writeMessage("Enter chat-name:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(TEXT, text));
+        } catch (IOException e) {
+            clientConnected = false;
+            ConsoleHelper.writeMessage("Connection error, user disconnected.");
+        }
+    }
 
     public class SocketThread extends Thread {
 
